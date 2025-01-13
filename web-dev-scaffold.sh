9048d02af6ec10e64d5eca4ae023c5cdbf8f67dd
philip <philipbreedlove@gmail.com>
2025-01-13 11:08:10 -0800

    updated gomponents install command

--- web-dev-scaffold.sh
+++ web-dev-scaffold.sh
@@ -7,83 +7,81 @@ read -p "Enter project name: " projectName
 
 echo "Creating ${projectName}"
 
-
 #-----------------------------------------
 #-----------------------------------------
 # create directories
 {
-mkdir "${projectName}"
-cd "${projectName}/"
+  mkdir "${projectName}"
+  cd "${projectName}/"
 
-mkdir cmd
-mkdir cmd/api
-mkdir cmd/api/domain
-mkdir cmd/api/domain/data_store
-mkdir cmd/api/domain/service
-mkdir cmd/api/domain/repository
-mkdir cmd/api/domain/handler
-mkdir cmd/api/domain/entity
-mkdir cmd/api/domain/components
-mkdir cmd/api/domain/components/shared
+  mkdir cmd
+  mkdir cmd/api
+  mkdir cmd/api/domain
+  mkdir cmd/api/domain/data_store
+  mkdir cmd/api/domain/service
+  mkdir cmd/api/domain/repository
+  mkdir cmd/api/domain/handler
+  mkdir cmd/api/domain/entity
+  mkdir cmd/api/domain/components
+  mkdir cmd/api/domain/components/shared
 
-mkdir static
-mkdir static/styles
-mkdir static/scripts
-mkdir db
-} &> /dev/null
+  mkdir static
+  mkdir static/styles
+  mkdir static/scripts
+  mkdir db
+} &>/dev/null
 echo "directories created"
 #-----------------------------------------
 #-----------------------------------------
 # install tools
 {
-go mod init "example.com/${projectName}"
+  go mod init "example.com/${projectName}"
 
-# GORM
-go get -u gorm.io/gorm
-go get -u gorm.io/driver/sqlite
+  # GORM
+  go get -u gorm.io/gorm
+  go get -u gorm.io/driver/sqlite
 
-# Echo web framework
-go get github.com/labstack/echo/v4
+  # Echo web framework
+  go get github.com/labstack/echo/v4
 
-# Gomponents
-go get github.com/maragudk/gomponents
+  # Gomponents
+  go get maragu.dev/gomponents
 
-# Tailwind
-curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
-chmod +x tailwindcss-linux-x64
-mv tailwindcss-linux-x64 tailwindcss
+  # Tailwind
+  curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
+  chmod +x tailwindcss-linux-x64
+  mv tailwindcss-linux-x64 tailwindcss
 
-# AIR live reload
-go install github.com/air-verse/air@latest
+  # AIR live reload
+  go install github.com/air-verse/air@latest
 
-# Flowbite UI
-wget https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css -P ./static/styles
-wget https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js -P ./static/scripts
+  # Flowbite UI
+  wget https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css -P ./static/styles
+  wget https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js -P ./static/scripts
 
-# HTMX
-wget https://unpkg.com/htmx.org@2.0.2/dist/htmx.min.js -P ./static/scripts
+  # HTMX
+  wget https://unpkg.com/htmx.org@2.0.2/dist/htmx.min.js -P ./static/scripts
 
-
-} &> /dev/null
+} &>/dev/null
 echo "tools installed"
 #-----------------------------------------
 #-----------------------------------------
 # create files
 {
-# environment variables
-cat > .env << EOF
+  # environment variables
+  cat >.env <<EOF
 DATABASE_URL=
 EOF
 
-# tailwind styles file for editing
-cat > input.css << END
+  # tailwind styles file for editing
+  cat >input.css <<END
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
 END
 
-# dev-server
-cat > makefile << END
+  # dev-server
+  cat >makefile <<END
 style-watcher:
 	./tailwindcss -i ./input.css -o ./static/styles/output.css --watch
 
@@ -92,8 +90,8 @@ dev-server:
 	air
 END
 
-# tailwind config
-cat > tailwind.config.js << END
+  # tailwind config
+  cat >tailwind.config.js <<END
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -106,8 +104,8 @@ module.exports = {
 }
 END
 
-# main.go
-cat > cmd/main.go << EOF
+  # main.go
+  cat >cmd/main.go <<EOF
 package main
 
 import (
@@ -134,8 +132,8 @@ func main() {
 }
 EOF
 
-# database
-cat > cmd/db.go << END
+  # database
+  cat >cmd/db.go <<END
 package main
 
 import (
@@ -148,14 +146,14 @@ func InitializeDatabase(dsn string) (*gorm.DB, error) {
 }
 END
 
-# shared page component
-cat > cmd/api/domain/components/shared/shared.go << END
+  # shared page component
+  cat >cmd/api/domain/components/shared/shared.go <<END
 package shared
 
 import (
-	g "github.com/maragudk/gomponents"
-	c "github.com/maragudk/gomponents/components"
-	. "github.com/maragudk/gomponents/html"
+	g "maragu.dev/gomponents"
+	c "maragu.dev/gomponents/components"
+	. "maragu.dev/gomponents/html"
 )
 
 func Page(title string, body g.Node) g.Node {
@@ -185,23 +183,22 @@ func SampleBody() g.Node {
 }
 END
 
-# sync go modules
-go mod vendor
+  # sync go modules
+  go mod vendor
 
-
-} &> /dev/null
+} &>/dev/null
 
 echo "files created"
 #-----------------------------------------
 #-----------------------------------------
 {
-# initialize git
-git init
-cat > .gitignore << EOF
+  # initialize git
+  git init
+  cat >.gitignore <<EOF
 .env
 tailwindcss
 EOF
-} &> /dev/null
+} &>/dev/null
 echo "git initialized"
 
 echo "Project is ready at ${projectName}/"
